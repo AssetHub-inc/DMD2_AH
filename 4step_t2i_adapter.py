83f026ebc1f69bc65c074f1fe912010fdb160047
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2024-12-13 19:50:49 -0800

    Adjustable parameters

--- 4step_t2i_adapter.py
+++ 4step_t2i_adapter.py
@@ -1,7 +1,8 @@
 from diffusers import DiffusionPipeline, StableDiffusionXLAdapterPipeline, T2IAdapter, AutoencoderKL, UNet2DConditionModel, LCMScheduler
-from diffusers.utils import load_image, make_image_grid
+from diffusers.utils import load_image
 from controlnet_aux.canny import CannyDetector
 from huggingface_hub import hf_hub_download
+from PIL import Image
 import torch
 
 
@@ -30,27 +31,34 @@ def prepare_pipe(
     return pipe
 
 
-def generate_image(pipe: DiffusionPipeline):
-    canny_detector = CannyDetector()
+def generate_image(
+    pipe: DiffusionPipeline,
+    image: str | Image.Image = "https://huggingface.co/Adapter/t2iadapter/resolve/main/figs_SDXLV1.0/org_canny.jpg",
+    prompt = "Mystical fairy in real, magic, 4k picture, high quality",
+    num_inference_steps=4,
+    guidance_scale=0,
+    adapter_conditioning_scale=0.8, 
+    adapter_conditioning_factor=0.5,
+    timesteps=[999, 749, 499, 249],
+    save_path="outputs/out_canny.png",
+):
 
-    url = "https://huggingface.co/Adapter/t2iadapter/resolve/main/figs_SDXLV1.0/org_canny.jpg"
-    image = load_image(url)
+    image = load_image(image)
+    canny_detector = CannyDetector()
 
     # Detect the canny map in low resolution to avoid high-frequency details
     image = canny_detector(image, detect_resolution=384, image_resolution=1024)#.resize((1024, 1024))
 
-    prompt = "Mystical fairy in real, magic, 4k picture, high quality"
-
-    gen_images = pipe(
+    gen_images: Image.Image = pipe(
         prompt=prompt,
         image=image,
-        num_inference_steps=4,
-        guidance_scale=0, 
-        adapter_conditioning_scale=0.8, 
-        adapter_conditioning_factor=0.5,
-        timesteps=[999, 749, 499, 249]
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+        adapter_conditioning_scale=adapter_conditioning_scale, 
+        adapter_conditioning_factor=adapter_conditioning_factor,
+        timesteps=timesteps,
     ).images[0]
-    gen_images.save('outputs/out_canny.png')
+    gen_images.save(save_path)
 
 
 if __name__ == "__main__":
