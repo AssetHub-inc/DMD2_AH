4c95cfeae1899bccf31edf3577ae7ec3a3d1ef9b
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2024-12-16 22:07:06 -0800

    Preprocessors `depth_midas` and `depth_zoe`

--- 4step_t2i_adapter.py
+++ 4step_t2i_adapter.py
@@ -1,14 +1,21 @@
 import os
 import time
+import typing
 from argparse import ArgumentParser, Namespace
+from typing import Literal, Union
 
 from diffusers import DiffusionPipeline, StableDiffusionXLAdapterPipeline, T2IAdapter, AutoencoderKL, UNet2DConditionModel, LCMScheduler
 from diffusers.pipelines.stable_diffusion_xl.pipeline_output import StableDiffusionXLPipelineOutput
 from diffusers.utils import load_image
 from controlnet_aux.canny import CannyDetector
+from controlnet_aux.zoe import ZoeDetector
+from controlnet_aux.midas import MidasDetector
 from huggingface_hub import hf_hub_download
 from PIL import Image
 import torch
+
+PREPROCESSOR = Union[CannyDetector, ZoeDetector, MidasDetector, None]
+PREPROCESSOR_NAME = Literal["canny", "depth_midas", "depth_zoe", "none"]
 
 
 def prepare_pipe(
@@ -37,8 +44,63 @@ def prepare_pipe(
     return pipe
 
 
+def load_preprocessors(
+    preprocessor_names: list[PREPROCESSOR_NAME] = ["canny"],
+    **kwargs,
+) -> list[PREPROCESSOR]:
+
+    print(f"Loading ControlNet preprocessors: {preprocessor_names}")
+    preprocessors = list()
+
+    for name in preprocessor_names:
+        if name == "canny":
+            preprosessor = CannyDetector()
+
+        elif name == "depth_midas":
+            preprosessor = MidasDetector.from_pretrained("lllyasviel/ControlNet").to("cuda")
+
+        elif name == "depth_midas":
+            preprosessor = ZoeDetector.from_pretrained("lllyasviel/ControlNet").to("cuda")
+
+        elif name == "none":
+            preprosessor = None
+
+        else:
+            raise ValueError(f"Preprocessor name: {name} is not in {PREPROCESSOR_NAME}")
+
+        preprocessors.append(preprosessor)
+
+    return preprocessors
+
+
+def preprocess_images(
+    image: Image.Image,
+    preprocessors: list[PREPROCESSOR],
+    image_resolution=1024,
+) -> list[Image.Image]:
+
+    preprocessed_images: list[Image.Image] = list()
+
+    for preprocessor in preprocessors:
+        if isinstance(preprocessor, CannyDetector):
+            # Detect the canny map in low resolution to avoid high-frequency details
+            preprocessed_img = preprocessor(image, detect_resolution=384,
+                                            image_resolution=image_resolution)#.resize((1024, 1024))
+
+        elif isinstance(preprocessor, (MidasDetector, ZoeDetector)):
+            preprocessed_img = preprocessor(image, image_resolution=image_resolution)
+
+        elif preprocessor is None:
+            preprocessed_img = image
+
+        preprocessed_images.append(preprocessed_img)
+
+    return preprocessed_images
+
+
 def generate_images(
     pipe: DiffusionPipeline,
+    preprocessors: list[PREPROCESSOR],
     image: str | Image.Image = "https://huggingface.co/Adapter/t2iadapter/resolve/main/figs_SDXLV1.0/org_canny.jpg",
     prompt = "Mystical fairy in real, magic, 4k picture, high quality",
     num_inference_steps=4,
@@ -51,15 +113,12 @@ def generate_images(
 ) -> list[Image.Image]:
 
     image = load_image(image)
-    canny_detector = CannyDetector()
-
-    # Detect the canny map in low resolution to avoid high-frequency details
-    image = canny_detector(image, detect_resolution=384, image_resolution=1024)#.resize((1024, 1024))
+    control_images = preprocess_images(image, preprocessors)
 
     begin = time.time()
     pipeline_output: StableDiffusionXLPipelineOutput = pipe(
         prompt=prompt,
-        image=image,
+        image=control_images,
         num_inference_steps=num_inference_steps,
         num_images_per_prompt=num_images_per_prompt,
         guidance_scale=guidance_scale,
@@ -89,10 +148,13 @@ def save_images(images: list[Image.Image], save_path="outputs/out_canny.png"):
 def parse_kwargs(omit_none=True) -> Namespace:
     parser = ArgumentParser()
 
+    parser.add_argument("--preprocessor_names", type=str, nargs="+",
+                        choices=typing.get_args(PREPROCESSOR_NAME)) # list[PREPROCESSOR_NAME]
     parser.add_argument("--prompt", type=str)
     parser.add_argument("--image", type=str)
     parser.add_argument("--num_inference_steps", "--steps", type=int)
     parser.add_argument("--num_images_per_prompt", "--batchsize", type=int)
+    parser.add_argument("--guidance_scale", "--cfg_scale", type=float)
 
     args = parser.parse_args()
     kwargs = vars(args)
@@ -104,10 +166,14 @@ def parse_kwargs(omit_none=True) -> Namespace:
 
 
 if __name__ == "__main__":
+    print(f"{PREPROCESSOR_NAME = }")
+    print(f"{vars(PREPROCESSOR_NAME) = }")
+    print(f"{typing.get_args(PREPROCESSOR_NAME) = }")
     kwargs = parse_kwargs()
     print(f"kwargs:\n{kwargs}")
 
     pipe = prepare_pipe(**kwargs)
-    gen_images = generate_images(pipe=pipe, **kwargs)
+    preprocessors = load_preprocessors(**kwargs)
+    gen_images = generate_images(pipe=pipe, preprocessors=preprocessors, **kwargs)
 
     save_images(gen_images)
